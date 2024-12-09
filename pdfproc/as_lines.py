854373a2827728c0705262e0eb53ad17b97dd6d3
Jester <jester@localhost>
2024-12-09 01:45:49 +0000

    Update extraction functions

--- pdfproc/as_lines.py
+++ pdfproc/as_lines.py
@@ -14,31 +14,33 @@ class Extractor:
         self.re_separator = re_separator
         self.re_hs = re_hs
 
-    def get_header(self,dataset,verbose=False):
+    def get_header(self,list_of_lines,verbose=False):
         ''' Find first header in the data '''
         header = {
             'start':None,
             'end':None
         }
-        for ln in range(len(dataset)):
-            if bool(re.search(self.re_hs,dataset[ln])) \
+        for ln,line in enumerate(list_of_lines):
+            if bool(re.search(self.re_hs,line)) \
                 and header['start'] == None:
-                if verbose: print(dataset[ln])
+                if verbose: print(line)
                 header['start'] = ln
-            elif bool(re.search(self.re_separator,dataset[ln])) \
+            elif bool(re.search(self.re_separator,line)) \
                 and header['end'] == None \
                 and header['start'] != None:
-                if verbose: print(dataset[ln])
+                if verbose: print(line)
                 header['end'] = ln
             elif header['start'] != None and header['end'] != None:
                 assert header['start'] != None
                 assert header['end'] != None
                 return header
+        raise ValueError(f"Cannot find header")
     
-    def get_page_data(self,dataset,strip_lines=True,verbose=False):
+    def get_page_data(self,list_of_lines,strip_lines=True,verbose=False):
+        ''' Pass page data without header, returns dictionary of elements '''
         page_data = {}; harvest = False
         collect = False
-        for line in dataset:
+        for line in list_of_lines:
             separator = None
             separator = re.search(self.re_separator,line)
             if separator:
@@ -52,22 +54,16 @@ class Extractor:
         if verbose: print(start,end)
         return page_data
 
-    def get_pages(self,file,verbose=False):
-        ''' Reads a file and returns list of pages as lists of lines.
-        Input filename to read from. '''
+    def get_pages(self,list_of_lines,verbose=False):
+        ''' Returns list of pages as lists of lines. '''
 
-        # Read file as list of binary strings, find all formfeed characters
-        with open(file, 'br') as f:
-            dataset = f.readlines()
+        # Find all formfeed characters
         formfeed_loc = []
         for ln,line in enumerate(dataset):
             if bool(re.search(b'\x0c',line)):
                 formfeed_loc.append(ln)
 
-        # Read file as list of common strings, record pages
-        with open(file, 'r') as f:
-            dataset = f.readlines()
-
+        # Record pages
         pages = []
         prev_loc = None
         for loc in formfeed_loc:
